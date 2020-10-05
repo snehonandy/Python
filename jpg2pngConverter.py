b28c2c3698746c2ad4974cd975c412051e062c60
snehonandy <72209652+snehonandy@users.noreply.github.com>
2020-10-05 19:48:22 +0530

    jpg to png image converter
        # How to run - python <scriptname><existing folder with jpg/><folder to be created with png/>
        # python jpg2pngConverter.py jpgs/ new/

---
+++ jpg2pngConverter.py
@@ -0,0 +1,19 @@
+import os
+import sys
+from PIL import Image
+
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+
+for filename in os.listdir(image_folder):
+    img = Image.open(f'{image_folder}{filename}')
+    clean_name = os.path.splitext(filename)[0]
+    img.save(f'{output_folder}{clean_name}.png','png')
+    print("Done...!!!!")
+
+    # How to run - python <scriptname><existing folder with jpg/><folder to be created with png/>
+    # python jpg2pngConverter.py jpgs/ new/
